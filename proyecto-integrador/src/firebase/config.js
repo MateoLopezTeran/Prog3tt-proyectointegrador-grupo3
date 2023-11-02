788a6aa5237dab7ffbae0b02d0a258c6401bbc82
Mateo Lopez Teran <mlopezteran@udesa.edu.ar>
2023-11-02 00:45:08 -0300

    cambios

--- proyecto-integrador/src/firebase/config.js
+++ proyecto-integrador/src/firebase/config.js
@@ -2,13 +2,13 @@ import app from 'firebase/app'
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBM0aywjgSRkYAp9itJ8MNYxDoSVyJ49DY",
-    authDomain: "proyectointegrador-d933c.firebaseapp.com",
-    projectId: "proyectointegrador-d933c",
-    storageBucket: "proyectointegrador-d933c.appspot.com",
-    messagingSenderId: "566295273585",
-    appId: "1:566295273585:web:68cd6fee9549e94c487bd0"
-  };
+  apiKey: "AIzaSyBM0aywjgSRkYAp9itJ8MNYxDoSVyJ49DY",
+  authDomain: "proyectointegrador-d933c.firebaseapp.com",
+  projectId: "proyectointegrador-d933c",
+  storageBucket: "proyectointegrador-d933c.appspot.com",
+  messagingSenderId: "566295273585",
+  appId: "1:566295273585:web:68cd6fee9549e94c487bd0"
+};
 
   app.initializeApp(firebaseConfig)
 
