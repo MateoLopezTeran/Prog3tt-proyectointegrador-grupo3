0c673957023e64bb23ee240d236e24bfd47a9f3e
AugustoAugier <augusaugier1212@gmail.com>
2023-11-08 15:41:51 -0300

    datos

--- proyecto-integrador/src/components/Menu.js
+++ proyecto-integrador/src/components/Menu.js
@@ -2,8 +2,6 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import react, { Component } from "react";
 import { TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList } from "react-native";
 import Home from "../screens/Home/Home";
-import Login from "../screens/Login/Login";
-import Register from "../screens/Register/Register";
 import Search from "../screens/Search/Search";
 import Profile from "../screens/Profile/Profile";
 import PostForm from "../screens/PostForm/PostForm";
