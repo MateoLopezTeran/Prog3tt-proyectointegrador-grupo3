a8a328fa56f71e86c7e44da26c82e5fda777ff18
AugustoAugier <augusaugier1212@gmail.com>
2023-11-13 17:07:40 -0300

    perfil detalles

--- proyecto-integrador/src/screens/Profile/Profile.js
+++ proyecto-integrador/src/screens/Profile/Profile.js
@@ -54,30 +54,35 @@ class Profile extends Component {
     console.log(this.state.profile);
     return (
       <View>
+      <br/>
+      <View style={styles.orden}>
+      <View style={styles.foto}>
+      <Text>Foto de Perfil: </Text>
+      { this.state.profile.length > 0 
+        ? 
+        <Text>{this.state.profile[0].data.foto}</Text>
+
+        : 
+        false}
+      </View>
+      <View style={styles.seccionProfile}>
+
+        <Text style = {styles.texto}>Usuario: {auth.currentUser.email}</Text>
+        { this.state.profile.length > 0 
+        ? 
+        <Text style = {styles.texto}>Nombre de Usuario: {this.state.profile[0].data.usuario}</Text>
+        : 
+        false}
+        { this.state.profile.length > 0           ? 
+        <Text style = {styles.texto}>Biografia: {this.state.profile[0].data.minibio}</Text>
+        : 
+        false}
+      </View>
+      </View>
+
+
         <br/>
-        <View style={styles.seccionProfile}>
-
-          <Text style = {styles.texto}>Usuario: {auth.currentUser.email}</Text>
-          { this.state.profile.length > 0 
-          ? 
-          <Text style = {styles.texto}>Foto de Perfil: {this.state.profile[0].data.foto}</Text>
-          : 
-          false}
-          { this.state.profile.length > 0 
-          ? 
-          <Text style = {styles.texto}>Nombre de Usuario: {this.state.profile[0].data.usuario}</Text>
-          : 
-          false}
-          { this.state.profile.length > 0 
-          ? 
-          <Text style = {styles.texto}>Biografia: {this.state.profile[0].data.minibio}</Text>
-          : 
-          false}
-        </View>
-
-
-        <br/>
-        <View>
+        <View style = {styles.seccionDos}>
         <Text style = {styles.texto}>Mis posteos:</Text>
         {/* {this.state.posteos === 0 
         ?
@@ -102,39 +107,47 @@ class Profile extends Component {
 }
 
 const styles = StyleSheet.create({
-  formContainer: {
-    paddingTop: 5,
-    paddingBottom: 10,
-    paddingLeft: 10,
-    margin: 15,
-    textAlign: "center",
-    backgroundColor: 'lightgrey',
-    borderRadius: 5,
-
+  orden: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  foto: {
+    width: 170,
+    height: 200,
   },
   seccionProfile: {
     flex: 1,
     flexDirection: 'column',
+    paddingBottom: 100,
+    height: 200,
     width: 200,
-    justifyContent: "center"
-},
+    justifyContent: "center",
+  },
+  seccionDos: {
+    paddingTop: 5,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    flex: 1,
+    justifyContent:"flex-start",
+  },
+  texto: {
+    textAlign: "left",
+    color: "black",
+    fontSize: 15,
+  },
   button: {
     backgroundColor: "red",
     paddingHorizontal: 10,
     paddingVertical: 6,
-    textAlign: "center",
+    justifyContent: "center",
     borderRadius: 4,
     borderWidth: 1,
     borderStyle: "solid",
     borderColor: "#28a745",
     margin: 5,
     width: 80,
-},
-  texto: {
-    textAlign: "left",
-    color: "black",
-    fontSize: 15,
-},
+  },
+
 });
 
 export default Profile;
